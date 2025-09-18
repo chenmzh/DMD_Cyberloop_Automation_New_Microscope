9ad5a8581f229e990831cd00532ba5b9f80f3473
chenmzh <mingzhchen@ethz.ch>
2025-09-18 17:36:26 +0200

    Complete (sub)Experiment date 2025-09-18 and time 05:36 PM

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -3,16 +3,16 @@ function [config_exp] = exp_config(imagingFolderName)
     %% CONFIGURATION PARAMETERS
     config_exp = [];
     %% EXPERIMENT SPECIFIC PARAMETERS
-    config_exp.experiment_name = 'Yeast_Git_';
+    config_exp.experiment_name = 'Yeast_Git';
     config_exp.time_date = '20250918';
     config_exp.time_hour = '132618';
     config_exp.organism = 'Yeast';
     config_exp.objective_type = '40x_oil';
     config_exp.magnification = '40x*1.5=60x';
     config_exp.strains = 'Nuclear translocation';
-    config_exp.initial_delay = 60*20; %% In seconds
-    config_exp.experiment_pattern_times = repmat([5,15],1,9)*60;
-    config_exp.experiment_pattern_values = repmat([1,0],1,9);
+    config_exp.initial_delay = 60*0; %% In seconds
+    config_exp.experiment_pattern_times = repmat([5,15],1,7)*1;
+    config_exp.experiment_pattern_values = repmat([1,0],1,7);
     config_exp.Period = 120;
     config_exp.intensity = 17.3;
     config_exp.light_normalization = 69.2;
@@ -21,6 +21,5 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.imaging.exposure = {10, 2000};
     config_exp.imaging.zOffsets = {[0], [0]};
     config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['Testing the shorter pattern with higher frequency, with 5 mins illumination and 15 mins darkness,\n ' ...
-    ' Still use the GUI so intensity does not make sense, set it to 50/255'];
+    config_exp.imaging.message = ['Testing telegram BOT'];
 end
