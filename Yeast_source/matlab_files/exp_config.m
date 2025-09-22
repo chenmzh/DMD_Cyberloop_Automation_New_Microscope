e8847b71c9ea470e2848ce712c000053662e41e1
chenmzh <mingzhchen@ethz.ch>
2025-09-22 20:05:21 +0200

    Complete (sub)Experiment date 2025-09-22 and time 08:05 PM

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -4,14 +4,14 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp = [];
     %% EXPERIMENT SPECIFIC PARAMETERS
     config_exp.experiment_name = 'Yeast_Git';
-    config_exp.time_date = '20250920';
-    config_exp.time_hour = '201618';
+    config_exp.time_date = '20250922';
+    config_exp.time_hour = '124912';
     config_exp.organism = 'Yeast';
     config_exp.objective_type = '40x_oil';
     config_exp.magnification = '40x*1.5=60x';
-    config_exp.strains = 'Nuclear translocation';
-    config_exp.initial_delay = 60*0; %% In seconds
-    config_exp.experiment_pattern_times = repmat([5,15],1,9)*1;
+    config_exp.strains = 'Bursty gene expression';
+    config_exp.initial_delay = 60*20; %% In seconds
+    config_exp.experiment_pattern_times = repmat([5,15],1,9)*60;
     config_exp.experiment_pattern_values = repmat([1,0],1,9);
     config_exp.Period = 120;
     config_exp.intensity = 17.3;
@@ -21,5 +21,5 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.imaging.exposure = {10, 2000};
     config_exp.imaging.zOffsets = {[0], [0]};
     config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['Test to connect image acquisitions with automatic analysis (new out folder: .../CyGenTiG_AUTO/), final data in ../CyGenTiG_PROCESSING/'];
+    config_exp.imaging.message = ['Trying a 5-15 pattern on the BGE cell line, this is the first measurement with an oil immersion objective for it.'];
 end
