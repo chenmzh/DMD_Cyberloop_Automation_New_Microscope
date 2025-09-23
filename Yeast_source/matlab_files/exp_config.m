4893dcf658cb218f675ac870fc8f467ae14913be
chenmzh <mingzhchen@ethz.ch>
2025-09-23 17:02:43 +0200

    Complete (sub)Experiment date 2025-09-23 and time 05:02 PM

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -9,10 +9,10 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.organism = 'Yeast';
     config_exp.objective_type = '40x_oil';
     config_exp.magnification = '40x*1.5=60x';
-    config_exp.strains = 'Bursty gene expression';
+    config_exp.strains = 'Mindy';
     config_exp.initial_delay = 60*20; %% In seconds
-    config_exp.experiment_pattern_times = repmat([5,15],1,9)*60;
-    config_exp.experiment_pattern_values = repmat([1,0],1,9);
+    config_exp.experiment_pattern_times = repmat( [10,1,20,2,20,4,20,8,20],1,2)*60;
+    config_exp.experiment_pattern_values = repmat([0 ,1,0, 1, 0,1, 0,1,0],1,2);
     config_exp.Period = 120;
     config_exp.intensity = 17.3;
     config_exp.light_normalization = 69.2;
@@ -21,5 +21,5 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.imaging.exposure = {10, 2000};
     config_exp.imaging.zOffsets = {[0], [0]};
     config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['Trying a 5-15 pattern on the BGE cell line, this is the first measurement with an oil immersion objective for it.'];
+    config_exp.imaging.message = ['Alternating and increasing pattern, repeated twice.'];
 end
