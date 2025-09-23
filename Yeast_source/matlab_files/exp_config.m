ef2340c42b6a3a3adffedb2479f62ff09337e65e
chenmzh <mingzhchen@ethz.ch>
2025-09-23 22:07:21 +0200

    Complete (sub)Experiment date 2025-09-23 and time 10:07 PM

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -4,14 +4,14 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp = [];
     %% EXPERIMENT SPECIFIC PARAMETERS
     config_exp.experiment_name = 'Yeast_Git';
-    config_exp.time_date = '20250922';
-    config_exp.time_hour = '124912';
+    config_exp.time_date = '20250923';
+    config_exp.time_hour = '171626';
     config_exp.organism = 'Yeast';
     config_exp.objective_type = '40x_oil';
     config_exp.magnification = '40x*1.5=60x';
     config_exp.strains = 'Mindy';
     config_exp.initial_delay = 60*20; %% In seconds
-    config_exp.experiment_pattern_times = repmat( [10,1,20,2,20,4,20,8,20],1,2)*60;
+    config_exp.experiment_pattern_times = repmat([10, 8,20,4,20,2,20,1,20],1,2)*60;
     config_exp.experiment_pattern_values = repmat([0 ,1,0, 1, 0,1, 0,1,0],1,2);
     config_exp.Period = 120;
     config_exp.intensity = 17.3;
@@ -21,5 +21,5 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.imaging.exposure = {10, 2000};
     config_exp.imaging.zOffsets = {[0], [0]};
     config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['Alternating and increasing pattern, repeated twice.'];
+    config_exp.imaging.message = ['Alternating and decreasing pattern, repeated twice.'];
 end
