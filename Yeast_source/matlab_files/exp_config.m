1b4a8a05781df2b34d3c2c0b47d88081b20ec1d9
chenmzh <mingzhchen@ethz.ch>
2025-09-24 11:48:08 +0200

    Create Experiment date 20250924 and time 114802, having a fresh start

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -11,8 +11,8 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.magnification = '40x*1.5=60x';
     config_exp.strains = 'Mindy';
     config_exp.initial_delay = 60*20; %% In seconds
-    config_exp.experiment_pattern_times = repmat([10, 8,20,4,20,2,20,1,20],1,2)*60;
-    config_exp.experiment_pattern_values = repmat([0 ,1,0, 1, 0,1, 0,1,0],1,2);
+    config_exp.experiment_pattern_times = repmat([10, 1,20,2,20,4,20,8,20,8,20,4,20,2,20,1,20],1,1)*60;
+    config_exp.experiment_pattern_values = repmat([0,  1,0, 1, 0,1, 0,1,0,1,0, 1, 0,1, 0,1,0],1,1);
     config_exp.Period = 120;
     config_exp.intensity = 17.3;
     config_exp.light_normalization = 69.2;
@@ -21,5 +21,5 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.imaging.exposure = {10, 2000};
     config_exp.imaging.zOffsets = {[0], [0]};
     config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['Alternating and decreasing pattern, repeated twice.'];
+    config_exp.imaging.message = ['Increasing and decreasing pattern.'];
 end
