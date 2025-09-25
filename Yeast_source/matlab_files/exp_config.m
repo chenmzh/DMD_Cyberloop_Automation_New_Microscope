09eab32a607cfcd4132e8f64b08005f1722f2eeb
chenmzh <mingzhchen@ethz.ch>
2025-09-25 15:52:33 +0200

    Complete (sub)Experiment date 2025-09-25 and time 03:52 PM

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -4,12 +4,12 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp = [];
     %% EXPERIMENT SPECIFIC PARAMETERS
     config_exp.experiment_name = 'Yeast_Git';
-    config_exp.time_date = '20250924';
-    config_exp.time_hour = '114802';
+    config_exp.time_date = '20250925';
+    config_exp.time_hour = '112702';
     config_exp.organism = 'Yeast';
     config_exp.objective_type = '40x_oil';
     config_exp.magnification = '40x*1.5=60x';
-    config_exp.strains = 'Mindy';
+    config_exp.strains = 'Bursty';
     config_exp.initial_delay = 60*20; %% In seconds
     config_exp.experiment_pattern_times = repmat([10, 1,20,2,20,4,20,8,20,8,20,4,20,2,20,1,20],1,1)*60;
     config_exp.experiment_pattern_values = repmat([0,  1,0, 1, 0,1, 0,1,0,1,0, 1, 0,1, 0,1,0],1,1);
