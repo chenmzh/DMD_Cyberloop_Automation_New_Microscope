69dc718b7e2a34b55f05c303d90aa1eb63590e0f
chenmzh <mingzhchen@ethz.ch>
2025-09-29 21:38:10 +0200

    Complete (sub)Experiment date 2025-09-29 and time 09:38 PM, with experiment message: shorter patterns to test limitation, with new 3-D printed plate holder, add PFS function and specific capturing function for implementation, PFS enabled, with new strain

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -9,7 +9,7 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.organism = 'Yeast';
     config_exp.objective_type = '40x_oil';
     config_exp.magnification = '40x*1.5=60x';
-    config_exp.strains = 'Bursty';
+    config_exp.strains = 'GE';
     config_exp.initial_delay = 60*20; %% In seconds
     config_exp.experiment_pattern_times = repmat([1200,10,1800,20,1800,30,1800,40,1800,50,1800,60,3200],1,1);
     config_exp.experiment_pattern_values = repmat([0,1,0,1,0,1,0,1,0,1,0,1,0],1,1);
@@ -21,6 +21,6 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.imaging.exposure = {10, 2000};
     config_exp.imaging.zOffsets = {[0], [0]};
     config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['shorter patterns to test limitation, with new 3-D printed plate holder, add PFS function and specific capturing function for implementation, PFS enabled'];
+    config_exp.imaging.message = ['shorter patterns to test limitation, with new 3-D printed plate holder, add PFS function and specific capturing function for implementation, PFS enabled, with new strain'];
     config_exp.UsingPFS = true;
 end
