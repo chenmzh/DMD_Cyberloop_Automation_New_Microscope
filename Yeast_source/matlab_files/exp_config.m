d991d9d776149c17edfde4b0aa95dd517fbad536
chenmzh <mingzhchen@ethz.ch>
2025-10-01 20:11:58 +0200

    Complete (sub)Experiment date 2025-10-01 and time 08:11 PM, with experiment message: Testing patterns with short bursts and ovelapping peaks, fast sampling 1 img/min, longer light pattern (120s), GE strain

--- Yeast_source/matlab_files/exp_config.m
+++ Yeast_source/matlab_files/exp_config.m
@@ -9,7 +9,7 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.organism = 'Yeast';
     config_exp.objective_type = '40x_oil';
     config_exp.magnification = '40x*1.5=60x';
-    config_exp.strains = 'BGE';
+    config_exp.strains = 'GE';
     config_exp.initial_delay = 60*20; %% In seconds
     config_exp.experiment_pattern_times = repmat([1200,120,120,120,240,120,480,120,920,120,1800,120,3200],1,1);	
     config_exp.experiment_pattern_values = repmat([0,1,0,1,0,1,0,1,0,1,0,1,0],1,1);
@@ -21,6 +21,6 @@ function [config_exp] = exp_config(imagingFolderName)
     config_exp.imaging.exposure = {10, 2000};
     config_exp.imaging.zOffsets = {[0], [0]};
     config_exp.imaging.condenser = {5, 5};
-    config_exp.imaging.message = ['Testing patterns with short bursts and ovelapping peaks, fast sampling 1 img/min, longer light pattern (120s)'];
+    config_exp.imaging.message = ['Testing patterns with short bursts and ovelapping peaks, fast sampling 1 img/min, longer light pattern (120s), GE strain'];
     config_exp.UsingPFS = true;
 end
